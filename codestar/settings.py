3aa56893dae989b97a02c84a616638289ab837ca
KieMerr17 <kieran.merrett@hotmail.com>
2023-05-16 08:59:46 +0000

    add admin post and comment management to admin.py file

--- codestar/settings.py
+++ codestar/settings.py
@@ -44,7 +44,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
+    'django_summernote',
     'blog',
 ]
 
